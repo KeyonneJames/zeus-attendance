6aefd5ab7dc9bf61f33dcff92112a0c7a6c0291c
keyonnejames <115831465+KeyonneJames@users.noreply.github.com>
2026-05-17 17:37:12 -0400

    update login message for regular users

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,7 +8,9 @@ function Login() {
 
         <div className="content-section">
           <p><strong>Login feature is coming soon</strong></p>
-          <p>For regular users, you do not need to log in to check in or out, just go to the home page and enter your name</p>
+          <p>
+            For regular users, you do not need to log in to check in or out, just go to the home page and enter your name. Hope you enjoy !
+          </p>
         </div>
       </div>
     </section>
