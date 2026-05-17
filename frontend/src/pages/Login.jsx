67751e82917b439f8c5dcd16d9a6f724259cbcff
keyonnejames <115831465+KeyonneJames@users.noreply.github.com>
2026-05-17 17:40:34 -0400

    fixing formatting issues in login page message

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
         <div className="content-section">
           <p><strong>Login feature is coming soon</strong></p>
           <p>
-            For regular users, you do not need to log in to check in or out, just go to the home page and enter your name. Hope you enjoy !
+            For regular users, you do not need to log in to check in or out, just go to the home page and enter your name. Hope you enjoy!
           </p>
         </div>
       </div>
